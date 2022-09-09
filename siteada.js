63374a8ed8fe393bcaa942a0ed6861c6806fecf5
Vanessa Oliveira <110072335+Vanessaor@users.noreply.github.com>
2022-09-09 20:19:50 -0300

    Add files via upload

---
+++ siteada.js
@@ -0,0 +1,20 @@
+
+//JS
+function validaFormulario()
+{
+
+    if
+    (document.getElementById("nome").value != "" &&
+        document.getElementById("email").value != "" &&
+        document.getElementById("telefone").value != "" 
+    )
+    {
+      alert("Prontinho! você receberá as novidades por email.")
+    }else
+    {
+       alert("Por favor, preencha os campos nome e email.")
+    }
+}
+   
+
+
